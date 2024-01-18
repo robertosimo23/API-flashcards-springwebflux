a238067cd086d7f7f9c445940d8bbcdcaa425c36
Roberto Simões <simoes9523@outlook.com>
2024-01-18 18:28:34 -0400

    First Commit

---
+++ src/test/java/dev/RobertoSimoes/reactiveflashcards/ReactiveFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.RobertoSimoes.reactiveflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
